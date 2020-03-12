d701dda6cf9a09d32c021d63950d8c1d58c863e8
Web Animate <60752454+webanimate@users.noreply.github.com>
2020-03-12 15:22:59 +0200

    build: update dist files

--- dist/lightSpeedOut.js
+++ dist/lightSpeedOut.js
@@ -1,3 +1,3 @@
 //animate.web v0.0.0-alpha.2 https://github.com/webanimate/animate.web#readme
-!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):((e=e||self).animateweb=e.animateweb||{},e.animateweb.lightSpeedOut=t())}(this,(function(){"use strict";return{keyframes:[{offset:0,transform:"translate3d(0, 0, 0)",opacity:1},{offset:1,transform:"translate3d(100%, 0, 0) skewX(30deg)",opacity:0}],options:{duration:1e3,easing:"ease-in"}}}));
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):((e=e||self).animateweb=e.animateweb||{},e.animateweb.lightSpeedOut=t())}(this,(function(){"use strict";return{keyframes:[{offset:0,opacity:1,transform:"translate3d(0, 0, 0)"},{offset:1,opacity:0,transform:"translate3d(100%, 0, 0) skewX(30deg)"}],options:{duration:1e3,easing:"ease-in"}}}));
 //# sourceMappingURL=lightSpeedOut.js.map
