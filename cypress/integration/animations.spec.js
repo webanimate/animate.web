bc157274e40afefcf20eee4584bcd8f062f11e5f
Web Animate <60752454+webanimate@users.noreply.github.com>
2020-03-16 16:07:15 +0200

    test: add cypress integration test

---
+++ cypress/integration/animations.spec.js
@@ -0,0 +1,26 @@
+import animations from '../../src/index'
+
+const animationsNames = []
+Object.keys(animations).forEach(key => {
+  if (!(key === 'categories' && !Array.isArray(animations[key].keyframes))) {
+    animationsNames.push(key)
+  }
+})
+
+describe('Test animations', function() {
+  it('should not trigger console errors', function() {
+    cy.visit('index.html')
+    cy.window().then(win => {
+      cy.spy(win.console, 'error')
+    })
+    for (const animation of animationsNames) {
+      cy.log(animation)
+      cy.get('#animationName input:first-child')
+        .clear()
+        .type(animation + '{uparrow}{enter}')
+      cy.window().then(win => {
+        expect(win.console.error).to.have.callCount(0)
+      })
+    }
+  })
+})
