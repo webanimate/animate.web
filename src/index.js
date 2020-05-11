6993350cd9179225e933930cc317415025f8cbc4
Web Animate <60752454+webanimate@users.noreply.github.com>
2020-05-12 01:43:10 +0200

    refactor: remove unnecessary keyframe from bounceInDown

--- src/index.js
+++ src/index.js
@@ -186,7 +186,6 @@ export default {
   bounceInDown: {
     keyframes: [
       { offset: 0.0, easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)', opacity: 0, transform: 'translate3d(0, -3000px, 0)', visibility: 'visible' },
-      { offset: 0.01, easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)', visibility: 'visible' },
       { offset: 0.6, easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)', opacity: 1, transform: 'translate3d(0, 25px, 0)' },
       { offset: 0.75, easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)', transform: 'translate3d(0, -10px, 0)' },
       { offset: 0.9, easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)', transform: 'translate3d(0, 5px, 0)' },
