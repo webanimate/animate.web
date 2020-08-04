2d5e616809983d6fe7c4c095e0a7b8e762904dad
Web Animate <60752454+webanimate@users.noreply.github.com>
2020-08-04 20:24:14 +0200

    perf: update devDependencies

--- dist/flash.js
+++ dist/flash.js
@@ -1,3 +1,3 @@
 //animate.web v1.0.1 https://github.com/webanimate/animate.web#readme
-!function(e,a){"object"==typeof exports&&"undefined"!=typeof module?module.exports=a():"function"==typeof define&&define.amd?define(a):((e=e||self).animateweb=e.animateweb||{},e.animateweb.flash=a())}(this,(function(){"use strict";return{keyframes:[{easing:"ease",opacity:1},{easing:"ease",opacity:0},{easing:"ease",opacity:1},{easing:"ease",opacity:0},{opacity:1}],options:{duration:1e3}}}));
+!function(e,i){"object"==typeof exports&&"undefined"!=typeof module?module.exports=i():"function"==typeof define&&define.amd?define(i):((e="undefined"!=typeof globalThis?globalThis:e||self).animateweb=e.animateweb||{},e.animateweb.flash=i())}(this,(function(){"use strict";return{keyframes:[{easing:"ease",opacity:1},{easing:"ease",opacity:0},{easing:"ease",opacity:1},{easing:"ease",opacity:0},{opacity:1}],options:{duration:1e3}}}));
 //# sourceMappingURL=flash.js.map
